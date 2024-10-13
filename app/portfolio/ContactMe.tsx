38ba7980c6e5b50f1dc0f203f26f9affa6672824
Dong Luu <Dluu520@yahoo.com>
2024-10-12 22:50:31 -0400

    Created: Base of ContactMe, Base of Projects Added: Pictures Related to Projects and Skills

--- app/portfolio/ContactMe.tsx
+++ app/portfolio/ContactMe.tsx
@@ -1,18 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 function ContactMe() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    // Handle form submission logic here
+
+    console.log("Name:", name);
+    console.log("Email:", email);
+  };
+
   return (
     <div
       id="contact"
       className="h-screen flex flex-col justify-center items-center p-10 md:p-52 gap-10 bg-gray"
     >
       <h1 className="text-4xl underline font-bold">Contact Me</h1>
-      <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odio
-        rerum maiores reprehenderit rem! Cumque labore cupiditate quia. Sequi
-        ullam quisquam itaque corporis distinctio impedit facilis dicta magnam
-        illo voluptate!
-      </span>
+      <div className="w-full p-4">
+        <form
+          action="mailto:dongluudev@gmail.com"
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-2 "
+        >
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            value={message}
+            id="message"
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button
+            type="submit"
+            className="p-2 border bg-white hover:cursor-pointer hover:bg-gray/50 "
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- app/portfolio/Projects.tsx
+++ app/portfolio/Projects.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 function Projects() {
@@ -7,9 +8,20 @@ function Projects() {
       className="h-screen flex flex-col justify-center items-center p-10 md:p-52 gap-10 bg-gray"
     >
       <h1 className="text-4xl underline font-bold">Projects</h1>
-      <div className="flex border border-black p-4 ">
-        <div>Image</div>
-        <div className="flex justify-center items-center ">Description</div>
+      <div className="flex border border-black overflow-hidden bg-white ">
+        <Image
+          width={400}
+          height={600}
+          alt=""
+          src="/p-trainer.png"
+          className=" overflow-x-hidden"
+        ></Image>
+        <div className="flex justify-center items-center text-balance">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet porro
+          alias labore officiis excepturi facilis nesciunt voluptatum optio
+          voluptatem delectus, sunt eos, facere consequuntur vel quos libero
+          deserunt doloremque mollitia!
+        </div>
       </div>
     </div>
   );
